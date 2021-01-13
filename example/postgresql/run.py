153f4421e1efb424d7c6abc9f194d98081b83b06
暁东邪 <xiaodongxiexie@users.noreply.github.com>
2021-01-13 15:30:58 +0800

    create: run sql process killer example

---
+++ example/postgresql/run.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+# @Author: xiaodong
+# @Date  : 2020/12/16
+
+import logging
+
+from autoSlowSQLKiller.sentinel import Sentinel
+from apscheduler.schedulers.background import BlockingScheduler
+
+logger = logging.getLogger("sql")
+
+
+if __name__ == '__main__':
+
+    logger.setLevel(logging.DEBUG)
+
+    sentinel = Sentinel()
+    sentinel.mock = True
+    sentinel.GLOBAL_ALLOW_MAX_ETL_SECONDS = 15
+
+    scheduler = BlockingScheduler()
+    scheduler.add_job(
+        func=sentinel.pipe,
+        args=["==allow-kill=True"],
+        trigger="interval", seconds=60,
+    )
+    scheduler.start()
