be0aec238115923bd611eadf544809fea60ed9de
暁东邪 <xiaodongxiexie@users.noreply.github.com>
2021-01-13 16:07:51 +0800

    Update run.py

--- example/postgresql/run.py
+++ example/postgresql/run.py
@@ -10,6 +10,21 @@ from apscheduler.schedulers.background import BlockingScheduler
 logger = logging.getLogger("sql")
 
 
+"""
+ -- forexample, you have a sql like this
+ 
+  -- ==allow-kill=True, allow-max-etl-seconds=600==
+  select xxx from xxxxx ...
+"""
+"""
+your sql comment should like '==allow-kill=True' or you defined something startswith '==' 
+as query label, by default query label must greater than 10 or get a ValueError if mock is False.
+when mock set as True, then kill process action just be logging but not execute.
+"""
+
+
+
+
 if __name__ == '__main__':
 
     logger.setLevel(logging.DEBUG)
